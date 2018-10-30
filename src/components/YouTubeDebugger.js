c0102e601eab8bbddfbc63d529f27cbd9c622f4e
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-30 14:31:33 -0500

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,56 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from "react";
+
+export default class YouTubeDebugger extends Component {
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: "1080p"
+      }
+    }
+  };
+
+  changeBitrate = () => {
+    this.setState(
+      {
+        settings: Object.assign({}, this.state.settings, {
+          bitrate: 12,
+          video: {
+            resolution: "1080p"
+          }
+        })
+      },
+      () => console.log("===>", this.state.settings.bitrate)
+    );
+  };
+
+  changeResolution = () => {
+    this.setState(
+      {
+        settings: Object.assign({}, this.state.settings, {
+          bitrate: 8,
+          video: {
+            resolution: "720p"
+          }
+        })
+      },
+      () => console.log("===>", this.state.settings.video.resolution)
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.changeBitrate}>
+          Bitrate
+        </button>
+        <button className="resolution" onClick={this.changeResolution}>
+          Resolution
+        </button>
+      </div>
+    );
+  }
+}
